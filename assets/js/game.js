0b379b493275b5c53a9e923fc87eab216050cc78
Kiranjot Kaur <kiranjotgahla@gmail.com>
2022-01-02 03:37:38 -0500

    console logs added

---
+++ assets/js/game.js
@@ -0,0 +1,43 @@
+function fight ()
+{
+    window.alert ("the fight has begun");
+}
+
+
+
+ var playerName = window.prompt("what is your robot's name ?") 
+ console.log("this logs a string, good for leaving yourself a message");
+
+ console.log(10 + 10);
+ console.log("our robots name is " + playerName);
+
+ console.log( playerName + " is ready for battle");
+ console.log ( "your robot, " + playerName + " ,has won the game");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ fight();
